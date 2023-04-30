5e81b5d772f0e5cf176e7d7e9a4c6cd25b5b0c82
Jay <blueriver.yu@gmail.com>
2023-04-30 16:16:35 +0900

    환율 표시시 내림 적용

--- core/common/src/main/kotlin/space/jay/wirebarley/core/common/format/Format.kt
+++ core/common/src/main/kotlin/space/jay/wirebarley/core/common/format/Format.kt
@@ -1,9 +1,13 @@
 package space.jay.wirebarley.core.common.format
 
+import java.math.RoundingMode
 import java.text.DecimalFormat
 import java.text.SimpleDateFormat
 import java.util.*
 
-fun Double.toPriceFormat(pattern : String = "#,##0.00") : String = DecimalFormat(pattern).format(this)
+fun Double.toPriceFormat(pattern : String = "#,##0.00") : String = DecimalFormat(pattern).let {
+    it.roundingMode = RoundingMode.DOWN
+    it.format(this)
+}
 
 fun Long.toDataFormat(pattern : String = "yyyy-MM-dd HH:mm") : String = SimpleDateFormat(pattern, Locale.getDefault()).format(Date(this))
