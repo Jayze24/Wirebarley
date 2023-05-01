8d0e8efc8d05cb1c8303002c5afc41b4b8db4b87
Jay <blueriver.yu@gmail.com>
2023-05-01 10:21:36 +0900

    코드 정리

--- feature/exchangeRateCalculation/src/main/kotlin/space/jay/wirebarley/feature/exchangeRateCalculation/NavigationExchangeRateCalculation.kt
+++ feature/exchangeRateCalculation/src/main/kotlin/space/jay/wirebarley/feature/exchangeRateCalculation/NavigationExchangeRateCalculation.kt
@@ -1,20 +1,12 @@
 package space.jay.wirebarley.feature.exchangeRateCalculation
 
-import androidx.activity.compose.BackHandler
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavOptions
-import androidx.navigation.NavType
 import androidx.navigation.compose.composable
-import androidx.navigation.navArgument
 
 const val routeExchangeRateCalculation = "routeExchangeRateCalculation"
 
